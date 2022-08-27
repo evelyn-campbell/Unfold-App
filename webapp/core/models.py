d0f136372e067a51060bfc059731324cb41d4fd6
Evelyn Campbell <eevelyncampbell@gmail.com>
2022-08-27 03:34:54 -0400

    Create user model

--- webapp/core/models.py
+++ webapp/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Users(models.Model):
+    username = models.CharField(max_length=24)
+    password = models.CharField(max_length=24)
+    email = models.EmailField(max_length=320)
+    private = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+
+
