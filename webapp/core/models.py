9cf3510fec36470698e03b9e8381b145bdc1e4a8
Evelyn Campbell <eevelyncampbell@gmail.com>
2022-08-27 05:02:23 -0400

    Add static file directory

--- webapp/core/models.py
+++ webapp/core/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 class Users(models.Model):
     username = models.CharField(max_length=24)
+    user_id = models.IntegerField()
     password = models.CharField(max_length=24)
     email = models.EmailField(max_length=320)
+    #profilepicture = models.ImageField(upload_to='profilepictures/')
     private = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
 
