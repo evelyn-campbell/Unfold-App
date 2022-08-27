680dd91f420c272a0ab02538a8e992f829fb9366
Evelyn Campbell <eevelyncampbell@gmail.com>
2022-08-27 09:50:38 -0400

    add page accessibility

--- webapp/core/urls.py
+++ webapp/core/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
-    path('profile/', views.profile, name='profile'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('profile/edit/avatar/', views.edit_avatar, name='edit_avatar'),
-    path('profile/edit/password/', views.edit_password, name='edit_password'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/create_post/', views.create_post, name='create_post'),
+    # path('profile/', views.profile, name='profile'),
+    # path('profile/edit/', views.edit_profile, name='edit_profile'),
+    # path('profile/edit/avatar/', views.edit_avatar, name='edit_avatar'),
+    # path('profile/edit/password/', views.edit_password, name='edit_password'),
+    # path('dashboard/', views.dashboard, name='dashboard'),
+    # path('dashboard/create_post/', views.create_post, name='create_post'),
 
 ]
 
