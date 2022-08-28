156719540d1651829779811198c45c545eec1ad2
Evelyn Campbell <eevelyncampbell@gmail.com>
2022-08-28 08:45:17 -0400

    hug works just needs ui lol

--- webapp/core/admin.py
+++ webapp/core/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Profile, Status, FriendRequest
+from .models import Profile, Status, FriendRequest, Hug
 
 admin.site.register(Profile)
 admin.site.register(Status)
 admin.site.register(FriendRequest)
+admin.site.register(Hug)
